dcbc1e0e65cad675a0bfa5dc12c76abfd0bd2321
arnonr <arnonr@kmutnb.ac.th>
2023-10-24 20:51:18 +0700

    gitignore

--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Applications/MAMP/htdocs/sicc/sicc-frontend/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "layout-eight" | "layout-eleven" | "layout-five" | "layout-four" | "layout-nine" | "layout-one" | "layout-seven" | "layout-six" | "layout-ten" | "layout-three" | "layout-twelve" | "layout-two" | "default"
-declare module "/Applications/MAMP/htdocs/sicc/sicc-frontend/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
